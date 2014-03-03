6712435c8afed89d1912b8b2a535e3732a313662
Joe G <wheeltirefamily@gmail.com>
2014-03-03 13:02:15 -0600

    init comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BitcoinApp::Application.config.secret_key_base = 'cd08698d1df9176834814a666be6b322def075a86c7a3d4d8c37496c08868fb1bfd21d8709e1630fbe79655bcfb7cb8ed4ba4236125984160668236e7f1b08ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BitcoinApp::Application.config.session_store :cookie_store, key: '_BitcoinApp_session'
