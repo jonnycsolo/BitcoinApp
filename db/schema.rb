938e75c32c0e927daa050eaa912a301a614d2cc9
Joe G <wheeltirefamily@gmail.com>
2014-03-05 23:34:02 -0600

    {{ product.image }} working

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,12 @@
 ActiveRecord::Schema.define(version: 20140306051541) do
 
   create_table "products", force: true do |t|
-    t.string   "image"
     t.string   "name"
     t.text     "desc"
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,16 @@
 Product.destroy_all
 
 Product.create(image: "FOR_F-305_BMF_.jpg" ,name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
-Product.create(image: "FOR_F-307_BMF_.jpg" ,name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
+Product.create(image: "FOR_F-306_BMF_.jpg" ,name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
+Product.create(image: "FOR_F-307_BMF_.jpg" ,name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
+Product.create(image: "FOR_F-308_BMF_.jpg" ,name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
+Product.create(image: "FOR_F-309_BMF_.jpg" ,name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
+Product.create(image: "FOR_F-310_BMF_.jpg" ,name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
+Product.create(image: "MER_M20_WMF_SSL_.jpg" ,name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
+Product.create(image: "MER_M35_BMF_SSL_.jpg" ,name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
+Product.create(image: "MER_M35_WMF_SSL_.jpg" ,name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
+Product.create(image: "MER_M38_BMF_SSL_.jpg" ,name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
+Product.create(image: "MER_M41_CHR_.jpg" ,name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
+Product.create(image: "MYST_802_CHR_.jpg" ,name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
+Product.create(image: "MYST_821_BMF_SSL_.jpg" ,name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
+Product.create(image: "MYST_821_CHR_.jpg" ,name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
