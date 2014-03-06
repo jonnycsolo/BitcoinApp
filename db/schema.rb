f25f1f39eef21fa7a4a4ae1ab46e589a94c3578f
Joe G <wheeltirefamily@gmail.com>
2014-03-05 23:17:07 -0600

    added img:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304070341) do
+ActiveRecord::Schema.define(version: 20140306051541) do
 
   create_table "products", force: true do |t|
+    t.string   "image"
     t.string   "name"
     t.text     "desc"
     t.integer  "price"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Product.destroy_all
 
-Product.create(name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
-Product.create(name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
-Product.create(name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
-Product.create(name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
-Product.create(name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
-Product.create(name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
-Product.create(name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
-Product.create(name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
-Product.create(name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
+Product.create(image: "FOR_F-305_BMF_.jpg" ,name: "Chrome 18 inch 5x114.3 FWD", desc: "Brand New Rims, set of 4, new tires would go great with this.", price: "599.99")
+Product.create(image: "FOR_F-307_BMF_.jpg" ,name: "245/40/18 New Tires", desc: "Brand New Tires, set of 4, new rims would go great with this.", price: "499.99")
